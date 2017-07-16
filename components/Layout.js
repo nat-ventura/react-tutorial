0cfbc3d15e7f38bbfdf5584d2db81d8bc6ff037b
Natalie Villasana <navillasa@gmail.com>
2017-07-15 23:23:40 -0400

    cut Layout and moved to components folder

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+class Layout extends React.Component {
+    constructor() {
+        super();
+        this.name = "Nat";
+    }
+}
+
+    render() {
+        return (
+            <h1>it's {this.name}!</h1>
+        )
+    }
