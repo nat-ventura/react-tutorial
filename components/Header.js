0e7d93718ca875fb8e13fd61346aa52312fe1ed0
Natalie Villasana <navillasa@gmail.com>
2017-07-15 23:42:50 -0400

    wrapped up basic components explanation

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
 
+import Title from "./Header/Title";
+
 export default class Header extends React.Component {
     render() {
         return (
-            <header>header</header>
+            <Title />
+            // even tho header returning the title,
+            // no extra dom elements added
+            // unless you wanna wrap a div over it
+            // passing values into components is up next
+            // and it's probs more complicated...
+            // maybe i will wait on that one
         )
     }
 }
